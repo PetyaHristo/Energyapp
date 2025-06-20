a939d1dbc4393298f76ef4baddda8f5b928a30d8
PetyaHristo <146973720+PetyaHristo@users.noreply.github.com>
2025-06-20 16:57:37 +0300

    Update db.php

--- db.php
+++ db.php
@@ -3,11 +3,18 @@ $host = "localhost";
 $db = "energy_app";
 $user = "root";
 $pass = ""; 
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Грешка при връзката с базата: " . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
 }
 ?>
